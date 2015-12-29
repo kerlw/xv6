9835c9ce7a9c009765fb0405390fbe7b57126e67
kerlw <kerl.d.w@gmail.com>
2015-12-29 11:49:16 +0800

    add system call halt

---
+++ halt.c
@@ -0,0 +1,17 @@
+/*************************************************************************
+	> File Name: halt.c
+	> Author: 
+	> Mail: 
+	> Created Time: 2015年12月28日 星期一 11时18分27秒
+ ************************************************************************/
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int
+main(int argc, char *argv[])
+{
+  halt();
+  return 0;
+}
